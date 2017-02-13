e1e9edd327c25d1a9d8295d39d8cdb53f4deb6e8
Kaiden Prince <me@kaidenprince.com>
2017-02-13 16:26:31 -0500

    base for login, any login works

--- src/webui/index.js
+++ src/webui/index.js
@@ -6,24 +6,65 @@
  * project for specific handling instructions
  */
 
-global.THREAD_NAME = process.env.GP_THREAD_NAME
-
-const d = require('../util/d')
-
 const express = require('express'),
-    path = require('path')
+    path = require('path'),
+    passport = require('passport'),
+    Strategy = require('passport-local').Strategy
+
+global.THREAD_NAME = process.env.GP_THREAD_NAME || 'main'
+global.PROJECT_ROOT = process.env.GP_PROJECT_ROOT || path.join(__dirname, '..', '..')
+
+const d = require('../util/d'),
+    secret = require('randomstring').generate()
 
 let app = express(),
     server = require('http').createServer(app),
     io = require('socket.io')(server)
 
+const session = require('express-session'),
+    NedbStore = require('nedb-session-store')(session)
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(session({secret: secret, resave: false, saveUninitialized: false, store: new NedbStore()}));
+
+passport.use(new Strategy({},
+    (username, password, cb) => {
+        d(username, password, cb)
+        cb(null, {id: 0})
+    }
+))
+
+passport.serializeUser(function (user, cb) {
+    cb(null, 0);
+});
+
+passport.deserializeUser(function (id, cb) {
+    cb(null, {id: 0});
+});
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+app.post('/login', (a, b, next) => {
+        d('post');
+        next()
+    },
+    passport.authenticate('local', {failureRedirect: '/login?incorrect=true'}),
+    function (req, res) {
+        res.redirect('/');
+    }
+)
+
+
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.use('/vendors', express.static(path.join(process.env.GP_PROJECT_ROOT, 'node_modules', 'gentelella', 'vendors')))
-app.use('/', express.static(path.join(process.env.GP_PROJECT_ROOT, 'node_modules', 'gentelella', 'build')))
-app.use('/css', express.static(path.join(process.env.GP_PROJECT_ROOT, 'node_modules', 'jquery.terminal', 'css')))
-app.use('/js', express.static(path.join(process.env.GP_PROJECT_ROOT, 'node_modules', 'jquery.terminal', 'js')))
-app.use('/js/keyboardevent-key-polyfill', express.static(path.join(process.env.GP_PROJECT_ROOT, 'node_modules', 'keyboardevent-key-polyfill')))
+app.use('/vendors', express.static(path.join(PROJECT_ROOT, 'node_modules', 'gentelella', 'vendors')))
+app.use('/', express.static(path.join(PROJECT_ROOT, 'node_modules', 'gentelella', 'build')))
+app.use('/css', express.static(path.join(PROJECT_ROOT, 'node_modules', 'jquery.terminal', 'css')))
+app.use('/js', express.static(path.join(PROJECT_ROOT, 'node_modules', 'jquery.terminal', 'js')))
+app.use('/js/keyboardevent-key-polyfill', express.static(path.join(PROJECT_ROOT, 'node_modules', 'keyboardevent-key-polyfill')))
 
 app.disable('view cache')
 app.set('view engine', 'pug')
@@ -48,7 +89,14 @@ let notifications = [
 ]
 
 app.get('/', function (req, res, next) {
-    res.render('index', {title: 'Home', notifications: notifications})
+    if (!req.user) {
+        res.redirect('/login')
+    } else {
+        res.render('index', {title: 'Home', notifications: notifications})
+    }
+})
+app.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Home', notifications: notifications})
 })
 app.get('/console', function (req, res, next) {
     res.render('console', {title: 'Console', notifications: notifications})
