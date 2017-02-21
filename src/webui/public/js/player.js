98691e534af529c26999569b90a8de0f3966a6fb
Kaiden Prince <me@kaidenprince.com>
2017-02-21 13:52:48 -0500

    timelines on player pages

---
+++ src/webui/public/js/player.js
@@ -0,0 +1,29 @@
+/**
+ * index
+ * This file was created on 20/02/17 by kkprince, and is
+ * a part of gunpowder. This file may be proprietary,
+ * however please se the LICENSE file in the root of this
+ * project for specific handling instructions
+ */
+let timeline
+$(() => {
+    let playerTimes = document.getElementById('playerTimes');
+    $.get(`/api/time/${uuid}`, undefined, time => {
+        time = new vis.DataSet(time)
+        timeline = new vis.Timeline(playerTimes, time, {
+            height: '100%'
+        });
+    })
+})
+function dateOp(d, days) {
+    d.setDate(d.getDate() + days);
+    return Math.floor(d.valueOf() / 1000)
+}
+$('#time').change(function () {
+    let start = dateOp(new Date(this.value), -2),
+        end = dateOp(new Date(this.value), 2)
+    $.get(`/api/time/${uuid}/${start}/${end}`, undefined, time => {
+        timeline.setItems(time)
+        timeline.fit()
+    })
+})
