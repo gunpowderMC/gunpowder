9ed48cffb92ff215117aab8a9dbb99af15f685f2
Kaiden Prince <me@kaidenprince.com>
2017-02-26 21:54:04 -0500

    start script

--- start.sh
+++ start.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-npm install
-docker-compose down
-docker-compose up -d --build
-chown :users run -R
-chmod 775 run -R
-chmod 600 run/authorized_keys
-chown 1000:1000 run/authorized_keys
+BASENAME=gp
+NAME=${BASENAME}_gunpowder
+MONGO=${BASENAME}_mongo
+
+docker stop $NAME $MONGO &> /dev/null
+docker rm $NAME $MONGO &> /dev/null
+docker network rm gunpowder
+BASEIP='172.2.0.'
+docker network create gunpowder --subnet "${BASEIP}0/16"
+docker run -d --restart always --name $MONGO \
+  --net gunpowder --ip ${BASEIP}2 \
+  -v $PWD/work/mongodb:/data/db \
+  mongo
+
+if [ "$DEV" == 'true' ]; then
+    echo "In dev mode"
+    docker build -t gunpowder -f Dockerfile-dev .
+else
+    docker build -t gunpowder .
+fi
+
+docker run -d --restart always --name ${NAME} \
+  -p "${MC_PORT:-25565}:25565" \
+  -p "${MC_PORT:-25565}:25565/udp" \
+  --net gunpowder --ip ${BASEIP}3 \
+  -p 80:8080 \
+  -v $PWD/work:/work \
+  -v $PWD:/app-dev \
+  --link ${MONGO}:mongod \
+  gunpowder
+
+exit 0
